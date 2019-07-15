4c726953322dc8828b40737934e4857e7a38aaef
MadhuriMungle <madhurimungle1012@gmail.com>
2019-07-15 17:18:15 +0530

    Recycle view 2 st Commit

--- app/src/main/java/com/example/recycleview_with_sqlite/nevigation_header.java
+++ app/src/main/java/com/example/recycleview_with_sqlite/nevigation_header.java
@@ -9,5 +9,7 @@ public class nevigation_header extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nevigation_header);
+
+        String Madhuri="Mvcfhjgfgjndjdgfj";
     }
 }
